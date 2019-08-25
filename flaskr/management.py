b70bcaf8bb8cb06c72709899794224c8a2f2731b
bhomerdodson <bhomerdodson@gmail.com>
2019-08-25 16:15:46 -0500

    Update management.py

--- flaskr/management.py
+++ flaskr/management.py
@@ -11,15 +11,16 @@ management = Blueprint('management', __name__, url_prefix='/management')
 
 @management.route('/create_game', methods=('POST'))
 def create_game():
-    name = request.form['name']
-    db = get_db()
-    
-    try:
-        if not name:
-            db.execute('INSERT INTO games (name) VALUES (?)', ('Game'))
-            db.commit()
-        else:
-            db.execute('INSERT INTO games (name) VALUES (?)', (name))
-            db.commit()
-    except sqlite3.Error as error:
-        error_message = 'Failed to insert new game' . error
+    if request.method == 'POST':
+        name = request.form['name']
+        db = get_db()
+        
+        try:
+            if not name:
+                db.execute('INSERT INTO games (name) VALUES (?)', ('Game'))
+                db.commit()
+            else:
+                db.execute('INSERT INTO games (name) VALUES (?)', (name))
+                db.commit()
+        except sqlite3.Error as error:
+            error_message = 'Failed to insert new game' . error
