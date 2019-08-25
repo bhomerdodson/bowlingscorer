cf8ceff6ea7153181f74a50341878e304406cbba
bhomerdodson <bhomerdodson@gmail.com>
2019-08-25 15:48:50 -0500

    Renamed directory

--- flaskr/scoring.py
+++ flaskr/scoring.py
@@ -4,6 +4,6 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for
 )
 
-from src.db import get_db
+from flaskr.db import get_db
 
 bp = Blueprint('scoring', __name__, url_prefix='/scoring')
