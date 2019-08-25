39a4c78bce25d021c73751b19fedfb5fd3d2edc5
bhomerdodson <bhomerdodson@gmail.com>
2019-08-25 16:18:11 -0500

    Trying to figure out blueprint

--- flaskr/management.py
+++ flaskr/management.py
@@ -7,20 +7,19 @@ from flask import (
 
 from flaskr.db import get_db
 
-management = Blueprint('management', __name__, url_prefix='/management')
+manage = Blueprint('management', __name__, url_prefix='/management')
 
-@management.route('/create_game', methods=('POST'))
+@manage.route('/create_game', methods=('POST'))
 def create_game():
-    if request.method == 'POST':
-        name = request.form['name']
-        db = get_db()
-        
-        try:
-            if not name:
-                db.execute('INSERT INTO games (name) VALUES (?)', ('Game'))
-                db.commit()
-            else:
-                db.execute('INSERT INTO games (name) VALUES (?)', (name))
-                db.commit()
-        except sqlite3.Error as error:
-            error_message = 'Failed to insert new game' . error
+    name = request.form['name']
+    db = get_db()
+    
+    try:
+        if not name:
+            db.execute('INSERT INTO games (name) VALUES (?)', ('Game'))
+            db.commit()
+        else:
+            db.execute('INSERT INTO games (name) VALUES (?)', (name))
+            db.commit()
+    except sqlite3.Error as error:
+        error_message = 'Failed to insert new game' . error
