f72cb178e0103a54af2c84cf5e250f181a3b40bb
bhomerdodson <bhomerdodson@gmail.com>
2019-08-25 16:51:06 -0500

    error handling

--- flaskr/management.py
+++ flaskr/management.py
@@ -22,4 +22,4 @@ def create_game():
             db.execute('INSERT INTO games (name) VALUES (?)', (name))
             db.commit()
     except sqlite3.Error as error:
-        error_message = 'Failed to insert new game' . error
+        error_message = 'Failed to insert new game. Error - {}' . str.format(error)
