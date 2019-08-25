450fea191cd0e06b759240ce92dca536331bab42
bhomerdodson <bhomerdodson@gmail.com>
2019-08-25 16:40:08 -0500

    Update index.py

--- flaskr/index.py
+++ flaskr/index.py
@@ -1,6 +1,8 @@
 import os
 
-from flask import Flask
+from flask import Flask, jsonify, request
+
+
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_mapping(
     SECRET_KEY='dev',
