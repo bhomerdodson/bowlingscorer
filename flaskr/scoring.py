f36ca0c6e2a66bd35f29321dc90bf4e7dd11336a
bhomerdodson <bhomerdodson@gmail.com>
2019-08-26 14:20:54 -0500

    Update scoring.py

--- flaskr/scoring.py
+++ flaskr/scoring.py
@@ -5,7 +5,7 @@ from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify
 )
 
-from flaskr.db import get_db
+from ..flaskr.db import get_db
 
 bp = Blueprint('scoring', __name__, url_prefix='/scoring')
 
