48dd9a6abde50b4f4b08d6c7d9a77346aa37c9de
bhomerdodson <bhomerdodson@gmail.com>
2019-08-26 15:16:24 -0500

    Added more cases

--- tests/test_management.py
+++ tests/test_management.py
@@ -87,3 +87,17 @@ def test_add_player_failures(client, manage, app):
     
     assert result['status'] == 400
     assert result['description'] == 'Did not give name of player'
+    
+    response = manage.add_player('Tom', '').data
+    
+    result = json.loads(response)
+    
+    assert result['status'] == 400
+    assert result['description'] == 'Did not give game id'
+    
+    response = manage.add_player('Bart', int(game_id) + 1).data
+    
+    result = json.loads(response)
+    
+    assert result['status'] == 400
+    assert result['description'] == 'Game does not exist'
