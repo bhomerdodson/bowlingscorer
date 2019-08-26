f283cf6f11775a5b588eeaa8a619b483719868a5
bhomerdodson <bhomerdodson@gmail.com>
2019-08-26 15:13:42 -0500

    Update test_management.py

--- tests/test_management.py
+++ tests/test_management.py
@@ -15,7 +15,7 @@ def test_create_delete_game(client, manage, app):
         assert row is not None
         assert row['count'] == 1
     
-    response = manage.delete_game().data
+    response = manage.delete_game(game_id).data
     result = json.loads(response)
     
     assert result['status'] == 200
@@ -58,7 +58,7 @@ def test_create_delete_player(client, manage, app):
         assert row is not None
         assert row['count'] == 0
 
-def test_add_player_failures(client, manage):
+def test_add_player_failures(client, manage, app):
     response = manage.create_game('Test').data
     result = json.loads(response)
     
