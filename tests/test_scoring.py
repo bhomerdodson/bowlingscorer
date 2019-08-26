17c6aef176c7305ecb998e1810d508e23523ab65
bhomerdodson <bhomerdodson@gmail.com>
2019-08-26 16:43:14 -0500

    Added get score failures test

--- tests/test_scoring.py
+++ tests/test_scoring.py
@@ -452,3 +452,62 @@ def test_get_score(client, scoring, manage, app):
     
     assert result['status'] == 200
     assert result['game_score'] == 8
+
+def test_get_score_failures(client, scoring, manage, app):
+    response = manage.create_game('Test').data
+    result = json.loads(response)
+    
+    assert result['status'] == 200
+    
+    game_id = result['game_id']
+    with app.app_context():
+        row = get_db().execute('SELECT count(*) as count FROM games WHERE id = ?', (game_id,)).fetchone()
+        assert row is not None
+        assert row['count'] == 1
+    
+    response = manage.add_player('Ben', game_id).data
+    result = json.loads(response)
+    
+    assert result['status'] == 200
+    
+    player_id = result['player_id']
+    with app.app_context():
+        row = get_db().execute('SELECT count(*) as count FROM players WHERE id = ?', (player_id,)).fetchone()
+        assert row is not None
+        assert row['count'] == 1
+    
+    response = scoring.add_frame(player_id, game_id).data
+    result = json.loads(response)
+    
+    assert result['status'] == 200
+    
+    frame_id = result['frameid']
+    with app.app_context():
+        row = get_db().execute('SELECT count(*) as count FROM frames WHERE id = ?', (frame_id,)).fetchone()
+        assert row is not None
+        assert row['count'] == 1
+    
+    response = scoring.update_frame(frame_id, 1, 5).data
+    result = json.loads(response)
+    
+    assert result['status'] == 200
+    assert result['description'] == 'Updated frame successfully'
+    
+    response = scoring.update_frame(frame_id, 2, 3).data
+    result = json.loads(response)
+    
+    assert result['status'] == 200
+    assert result['description'] == 'Updated frame successfully'
+    
+    response = scoring.get_score('').data
+    result = json.loads(response)
+    
+    assert result['status'] == 400
+    assert result['game_score'] == 'Did not give a frame id'
+    
+    response = scoring.get_score(5000).data
+    result = json.loads(response)
+    
+    assert result['status'] == 400
+    assert result['game_score'] == 'Did not give a valid frame id'
+
